9929e7ae27edc0bc456ea5e8cda88f5c18f7bdcc
jiyoul <wldbf97@naver.com>
2020-09-16 22:50:14 +0900

    Kakao Map edit

--- src/components/mains/Map.js
+++ src/components/mains/Map.js
@@ -1,14 +1,15 @@
-import { map } from "jquery";
 /*global kakao*/
+import { map } from "jquery";
 import React, { Component } from "react"
+import API from "../../config/apikey.json"
 class Map extends Component {
   componentDidMount(){
     const lat = this.props.lat;
     const lng = this.props.lng;
-
+    const API_KEY = API.kakaoMapAPI.API_KEY;
     const mapScript = document.createElement("script");
     mapScript.async = true;
-    mapScript.src = "https://dapi.kakao.com/v2/maps/sdk.js?appkey=f0e1982bec41decdb43b25d83973abbd&autoload=false";
+    mapScript.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${API_KEY}&autoload=false`;
     document.head.appendChild(mapScript);
     mapScript.onload = () => {
       kakao.maps.load(() => {
