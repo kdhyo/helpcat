fb8384c5a83e8bc6bfa6855d471f2e849a1503bc
Choi JiYoul <wldbf97@naver.com>
2020-10-08 14:58:56 +0900

    gogogo

--- src/components/messages/MessageUserDetail.js
+++ src/components/messages/MessageUserDetail.js
@@ -10,10 +10,12 @@ class MessageUserDetail extends Component {
       const roomId = opponentData.UserOnRoom[0].roomId
       return (
         <>
+        <Link to={"/message/",{roomId}}>
           <div className="chatting-box">
             <li className="chatting-user">{opponentData.UserOnRoom[0].user[0] ==  meData.id? opponentData.UserOnRoom[1].user[0].nickName : opponentData.UserOnRoom[0].user[0].nickName}</li><br></br>
             {/* <li className="chatting-text">{roomId}</li> */}
           </div>
+        </Link>
         </>
       );
       }else{
